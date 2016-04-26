903454f2691ea0946c2b9a68e219b215169b5196
myticmoon <vietnamknight2005@gmail.com>
2016-04-27 00:43:02 +0800

    Working S3 upload

--- client/views/cards/card_edit.js
+++ client/views/cards/card_edit.js
@@ -35,3 +35,22 @@ Template.cardEdit.events({
             Router.go('cardsList', {_id: currentDeckId});
     } }
 });
+
+Template.s3_tester.events({
+    "click button.upload": function(){
+        var files = $("input.file_bag")[0].files;
+
+        S3.upload({
+            files:files,
+            path:"subfolder"
+        },function(e,r){
+            console.log(r);
+        });
+    }
+});
+
+Template.s3_tester.helpers({
+    "files": function(){
+        return S3.collection.find();
+    }
+});
---
+++ server/settings.js
@@ -0,0 +1,6 @@
+S3.config = {
+    key: 'AKIAIGVI24E2NTIYWN3Q',
+    secret: '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',
+    bucket: 'flashxcard',
+    region: 'ap-southeast-1'
+};
