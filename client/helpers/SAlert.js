e88cec17dfe7a5b943c4fb80fff9db4d60a8d686
myticmoon <vietnamknight2005@gmail.com>
2016-07-23 19:37:55 +0800

    Minor change - before adding Stripe

--- client/helpers/SAlert.js
+++ client/helpers/SAlert.js
@@ -2,7 +2,7 @@
     sAlert.config({
         effect: '',
         position: 'bottom',
-        timeout: 5000,
+        timeout: 2000,
         html: false,
         onRouteClose: true,
         stack: true,
@@ -11,7 +11,7 @@
         //     spacing: 10 // in px
         //     limit: 3 // when fourth alert appears all previous ones are cleared
         // }
-        offset: 0, // in px - will be added to first alert (bottom or top - depends of the position in config)
+        offset: 2, // in px - will be added to first alert (bottom or top - depends of the position in config)
         beep: false,
         // examples:
         // beep: '/beep.mp3'  // or you can pass an object:
